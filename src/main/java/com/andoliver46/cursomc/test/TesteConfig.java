bc3ad9dec7dc81031788218eb152cd081063e550
AndOliver46 <anderson_souza75@live.com>
2022-11-04 14:09:43 -0300

    produto e associacao muitos para muitos

--- src/main/java/com/andoliver46/cursomc/test/TesteConfig.java
+++ src/main/java/com/andoliver46/cursomc/test/TesteConfig.java
@@ -7,21 +7,38 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.andoliver46.cursomc.domain.Categoria;
+import com.andoliver46.cursomc.domain.Produto;
 import com.andoliver46.cursomc.repositories.CategoriaRespository;
+import com.andoliver46.cursomc.repositories.ProdutoRepository;
 
 @Configuration
 public class TesteConfig implements CommandLineRunner {
 
 	@Autowired
-	CategoriaRespository repo;
-	
+	CategoriaRespository categoriaRepository;
+
+	@Autowired
+	ProdutoRepository produtoRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
 
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
 		
-		repo.saveAll(Arrays.asList(cat1,cat2));
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
 	}
 
