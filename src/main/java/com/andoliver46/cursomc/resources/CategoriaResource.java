ca2275dccb833d550c5186dc4664aea3c98d3882
AndOliver46 <anderson_souza75@live.com>
2022-11-04 12:13:22 -0300

    Testando h2 db

--- src/main/java/com/andoliver46/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/andoliver46/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,24 @@
 package com.andoliver46.cursomc.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.andoliver46.cursomc.dominio.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest esta funcionando";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = Arrays.asList(cat1, cat2);
+		return lista;
 	}
 }	
