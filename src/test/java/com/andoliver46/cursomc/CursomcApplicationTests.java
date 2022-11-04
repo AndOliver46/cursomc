0710672b6066891f5e2a0ae62a7cf605027db06f
AndOliver46 <anderson_souza75@live.com>
2022-11-04 11:10:58 -0300

    Projeto criado

---
+++ src/test/java/com/andoliver46/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andoliver46.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
