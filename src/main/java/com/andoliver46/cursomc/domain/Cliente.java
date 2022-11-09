002a84a0009dfeb77c4b17e0ac08aa52c9b87326
AndOliver46 <anderson_souza75@live.com>
2022-11-09 11:24:13 -0300

    Entidades Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/andoliver46/cursomc/domain/Cliente.java
+++ src/main/java/com/andoliver46/cursomc/domain/Cliente.java
@@ -33,7 +33,10 @@ public class Cliente implements Serializable {
 	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
-
+	
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	@ElementCollection
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
@@ -96,6 +99,14 @@ public class Cliente implements Serializable {
 
 	public void setEnderecos(List<Endereco> enderecos) {
 		this.enderecos = enderecos;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 
 	public Set<String> getTelefones() {
